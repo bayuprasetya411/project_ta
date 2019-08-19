de454905e117e1446749088aff27ed2605affd92
bayuprasetya411 <bayuprasetya411@gmail.com>
2019-08-19 23:39:46 +0800

    login done

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
 
 </head>
 
-<body class="login" style="background-color:grey">
+<body class="login" style="background-color:grey;">
     <div class="login_wrapper">
         <div class="card-body">
 
@@ -58,33 +58,36 @@
                     </div>
 
                     <div class="separator"></div>
-                        <div style="background:red; border:black; color:white; padding:2%; width:80%; border-radius:3%; position: relative; margin-left:10%; opacity:0.9;"><h3>Login</h3></div>
-                            <div style="background:white; border:black; color:black; padding:2%; border-radius:3%; margin-top:-8%; ">
-                                <div style="background:white; border:black; color:black; border-radius:2%; margin-top:15%">
-                                    <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
-                                        <input type="text" class="form-control has-feedback-left" id="username" name="username" placeholder="Username" required="" autofocus="autofocus" />
-                                        <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                    <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
-                                        <input type="password" class="form-control has-feedback-left" id="password" name="password" class="form-control" placeholder="Password" required="" />
-                                        <span class="fa fa-lock form-control-feedback left" aria-hidden="true"></span>
-                                    </div>
-                                        <button class="btn btn-primary btn-block" name="submit" id="tombollogin" type="submit" style="color:antiquewhite; background-color:red; width:94%; margin-left:3%">Login</button>
-                                    <div style="margin-top:10%"></div>
-                                </div>
+                    <div style="background:red; border:black; color:white; padding:2%; width:90%; border-radius:3%; position: relative; margin-left:5%; 
+                    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56), 0 4px 25px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);">
+                        <h5>Log On Application</h5>
+                    </div>
+                    <div style="background:white; border:black; color:black; padding:2%; border-radius:3%; margin-top:-8%;">
+                        <div style="background:white; border:black; color:black; border-radius:2%; margin-top:15%">
+                            <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                                <input type="text" class="form-control has-feedback-left" id="username" name="username" placeholder="Username" required="" autofocus="autofocus" />
+                                <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
                             </div>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-
-                    <div class="separator">
-
-                        <div style="text-align:center; color:white; mar">
-                            <p>© 2019 All Rights Reserved. SPK Teknisi Corporate Service</p>
+                            <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                                <input type="password" class="form-control has-feedback-left" id="password" name="password" class="form-control" placeholder="Password" required="" />
+                                <span class="fa fa-lock form-control-feedback left" aria-hidden="true"></span>
+                            </div>
+                            <button class="btn btn-primary btn-block" name="submit" id="tombollogin" type="submit" style="color:antiquewhite; background-color:red; width:94%; margin-left:3%">Login</button>
+                            <div style="margin-top:10%"></div>
                         </div>
                     </div>
-            </form>
+                </div>
+                <div class="clearfix"></div>
         </div>
+
+        <div class="separator">
+
+            <div style="text-align:center; color:white; mar">
+                <p>© 2019 All Rights Reserved. SPK Teknisi Corporate Service</p>
+            </div>
+        </div>
+        </form>
+    </div>
     </div>
 
     <!-- jQuery -->
